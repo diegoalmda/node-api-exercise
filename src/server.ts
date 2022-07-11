b5b56f91b49eb8119d814da28619746cbf632776
Diego Almeida <diegoalmda@gmail.com>
2022-07-11 11:45:10 -0300

    Created model Phrase

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from "express";
 import path from 'path';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import apiRoutes from './routes/api';
 
 dotenv.config();
 
 const server = express();
 
+server.use(cors({
+  origin: '*',
+  methods: ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'DELETE', 'PATCH'],
+}));
+
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({extended: true}));
+
+server.use('/api', apiRoutes)
 
 server.use((request: Request, response: Response) => {
   response.status(404);
